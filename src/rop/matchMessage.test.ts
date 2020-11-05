74f953ebcec4d53f12833339dcef523fac6b8588
chrisslater <chris@snapper.fish>
2020-11-05 18:33:19 +0000

    fix: add id capture to matchMessage

--- src/rop/matchMessage.test.ts
+++ src/rop/matchMessage.test.ts
@@ -2,11 +2,15 @@ import { matchMessage } from './matchMessage'
 import { fail } from './result'
 
 const messages = {
+    WithIdErrorMessage: jest.fn(),
+    Error: jest.fn(),
     ErrorMessage: jest.fn(),
 }
 
 describe('matchMessage', () => {
     afterEach(() => {
+        messages.Error.mockReset()
+        messages.WithIdErrorMessage.mockReset()
         messages.ErrorMessage.mockReset()
     })
 
@@ -15,18 +19,39 @@ describe('matchMessage', () => {
             matchMessage(messages)(fail({ code: 'ErrorMessage' }))
         })
 
-        it('should call function', () => {
-            expect(messages.ErrorMessage).toBeCalled()
+        it('should not call WithIdError function', () => {
+            expect(messages.WithIdErrorMessage).toBeCalledTimes(0)
+        })
+
+        it('should call ErrorMessage function', () => {
+            expect(messages.ErrorMessage).toBeCalledTimes(1)
+        })
+
+        it('should call Error function', () => {
+            expect(messages.Error).toBeCalledTimes(0)
         })
     })
 
-    describe('when a matching message is passed', () => {
+    describe('when a matching message with `id` is passed', () => {
         beforeEach(() => {
-            matchMessage(messages)(fail({ code: 'Error' }))
+            matchMessage(messages)(fail({ id: 'WithId', code: 'ErrorMessage' }))
         })
 
-        it('should call function', () => {
+        it('should call WithIdError function', () => {
+            expect(messages.WithIdErrorMessage).toBeCalledTimes(1)
+        })
+
+        it('should not call ErrorMessage function', () => {
             expect(messages.ErrorMessage).toBeCalledTimes(0)
         })
+
+        it('should not call Error function', () => {
+            expect(messages.Error).toBeCalledTimes(0)
+        })
+    })
+
+    describe('when no matching error message is passed', () => {
+        
+        it.todo('should call generic Error function')
     })
 })
--- src/rop/matchMessage.ts
+++ src/rop/matchMessage.ts
@@ -3,6 +3,8 @@ import { isFail } from './result'
 
 export const matchMessage: MatchMessage = (messages) => (result) => {
     isFail(result) && result.messages.map((message) => {
-        messages && messages[message.code] && messages[message.code](message)
+        const identifier = `${message.id || ''}${message.code}`
+        // messages && messages[message.code] && messages[message.code](message)
+        messages && messages[identifier] && messages[identifier](message)
     })
 }
