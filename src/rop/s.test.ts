bf282037010346a7d2b1c08a8be9d81964db8908
chrisslater <chris@snapper.fish>
2020-11-05 16:00:46 +0000

    fix: broken test

--- src/rop/s.test.ts
+++ src/rop/s.test.ts
@@ -7,19 +7,18 @@ describe.skip('s', () => {
 
     describe('when a fail is passed', () => {
         it('should not run the function', () => {
-            const mockFail = fail<string>('hello')
+            const mockFail = fail<string>({ code: 'hello'})
             const result = successTreeFn(mockFail)
 
             expect(mockFn).toBeCalledTimes(0)
             expect(result).toBeInstanceOf(Fail)
-            expect(result.value).toEqual(['hello'])
+            expect(result.value).toEqual([{ code: 'hello' }])
         })
     })
 
     describe('when a success is passed', () => {
         it('should run the function', () => {
             const mockSuccess = succeed<string>('hello')
-            mockFn.mockReturnValue
             const result = successTreeFn(mockSuccess)
 
             expect(mockFn).toBeCalledTimes(1)
