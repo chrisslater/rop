21766ae8a248db1723981842cfa67a4f4615dbeb
Christopher Slater <c.slater@fairfx.com>
2020-11-14 12:48:13 +0000

    fix: hide async

--- src/rop/experimental/toAsync.test.ts
+++ src/rop/experimental/toAsync.test.ts
@@ -1,15 +1,15 @@
 import { Result } from '../types'
 import { succeed } from '../result'
-import { toAsync } from './toAsync'
+// import { toAsync } from './toAsync'
 
 describe('asyncWrap', () => {
     describe('', () => {
         it.skip('should', () => {
-            const test = (a: Result<string>): string => {
-                return 'foo'
-            }
+            // const test = (a: Result<string>): string => {
+            //     return 'foo'
+            // }
 
-            const meh = toAsync(test)(Promise.resolve(succeed('hello')))
+            // const meh = toAsync(test)(Promise.resolve(succeed('hello')))
         })
     })
 })
