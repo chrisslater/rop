a2ad93496194c7d196a21f5b07ef9a2da2dfa166
Christopher Slater <c.slater@fairfx.com>
2020-11-16 09:49:25 +0000

    fix: add value to matchResult

--- src/rop/matchMessage.ts
+++ src/rop/matchMessage.ts
@@ -1,24 +1,24 @@
 import { MessageEnvelope, Result } from './types'
 import { isSuccess, isFail } from './result'
 
-export type Messages<ReturnValue> =  {
-    [str: string]: (input: { message: MessageEnvelope }) => ReturnValue
-    Default: (input: { message: MessageEnvelope }) => ReturnValue
+export type Messages<ResultValue, ReturnValue> =  {
+    [str: string]: (input: { value: ResultValue | undefined; message: MessageEnvelope }) => ReturnValue
+    Default: (input: { value: ResultValue | undefined; message: MessageEnvelope }) => ReturnValue
 }
 
-export const matchMessage = <ReturnValue>(messages: Messages<ReturnValue>) => <A>(result: Result<A>): ReturnValue  => {
+export const matchMessage = <ResultValue, ReturnValue>(messages: Messages<ResultValue, ReturnValue>) => (result: Result<ResultValue>): ReturnValue  => {
     const message = result.messages && result.messages[0]
     const identifier = message && `${message.id || ''}${message.code}` || ''
 
     if (identifier in messages) {
-        return messages[identifier]({ message })
+        return messages[identifier]({ value: result.value, message })
     } else if (message && message.code in messages) {
-        return messages[message.code]({ message })
+        return messages[message.code]({ value: result.value, message })
     } else if (isSuccess(result) && 'Success' in messages) {
-        return messages.Success({ message })
+        return messages.Success({ value: result.value, message })
     } else if (isFail(result) && 'Fail' in messages) {
-        return messages.Fail({ message })
+        return messages.Fail({ value: result.value, message })
     } else {
-        return messages.Default({ message })
+        return messages.Default({ value: result.value, message })
     }
 }
